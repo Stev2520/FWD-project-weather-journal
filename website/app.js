a318eaaad49a1733a382f771f60f38053f2aedf1
Stev2520 <62143139+Stev2520@users.noreply.github.com>
2021-07-10 15:31:54 +0200

    add units parameter to openweathermap get request

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-//OpenWeathermap API key
+//OpenWeathermap API
+const apiBase = "https://api.openweathermap.org/data/2.5/weather?units=metric&zip=";
 const apiKey = "42759c50ec83a021669181d38d7ace61";
 
 // Inputs
@@ -15,9 +16,9 @@ const lastTemp = document.getElementById("temp");
 // Show last entry
 (async function() {
     const lastEntry = await fetch("/all");
-    lastDate.textContent = lastEntry.lastDate;
-    lastTemp.textContent = lastEntry.lastTemp;
-    lastFeelings.textContent = lastFeelings.lastDate;
+    lastDate.innerHTML = lastEntry.lastDate;
+    lastTemp.innerHTML = lastEntry.lastTemp;
+    lastFeelings.innerHTML = lastFeelings.lastDate;
 })();
 
 
@@ -26,13 +27,13 @@ document.getElementById("generate").addEventListener("click",generateEntry);
 async function generateEntry(){
     let d = new Date();
     let data = {};
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}`).json();
-    data.lastTemp = (response.main.temp - 273.15).toFixed(2) + " °C";
+    let response = await fetch(apiBase + zipCode.value + "&appid=" + apiKey).then(res => res.json());
+    data.lastTemp = (response.main.temp + " °C";
     data.lastDate = `${1+d.getMonth()}.${d.getDate()}.${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
     data.lastFeelings = feelings.value;
-    lastDate.textContent = data.lastDate;
-    lastTemp.textContent = data.lastTemp;
-    lastFeelings.textContent = data.lastFeelings;
+    lastDate.innerHTML = data.lastDate;
+    lastTemp.innerHTML = data.lastTemp;
+    lastFeelings.innerHTML = data.lastFeelings;
     await fetch("/entry",{
         method: "POST",
         credentials: "same-origin",
